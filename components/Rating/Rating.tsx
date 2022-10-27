0c93fb7adc524d8701da96decaaaef7098cda955
Sergey Neskin <neskin.x@mail.ru>
2022-10-27 22:16:21 +0700

    добавлена возможность обработка наведения, клика, ховера и навигации по клавиатуре на иконки рейтинга

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -2,7 +2,7 @@ import styles from './Rating.module.css';
 import { RatingProps } from './Rating.props';
 import cn from 'classnames';
 import StarIcon from './star.svg';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, KeyboardEvent } from 'react';
 
 export const Rating = ({isEditable = false, rating, setRating, children, ...props }: RatingProps): JSX.Element => {
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
@@ -14,12 +14,42 @@ export const Rating = ({isEditable = false, rating, setRating, children, ...prop
 	const constructRating = (currentRating: number) => {
 		const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
 			return (
-				<StarIcon key={i} className={cn(styles.star, {
-					[styles.filled]: i < currentRating
-				})} />
+				<span 
+					key={i} 
+					className={cn(styles.star, {
+						[styles.filled]: i < currentRating,
+						[styles.editable]: isEditable
+					})} 
+					onMouseEnter={(): void => changeDisplay(i + 1)} 
+					onMouseLeave={(): void => changeDisplay(rating)}
+					onClick={(): void => onclick(i + 1)}
+					>
+					<StarIcon 
+						tabIndex={isEditable ? 0 : -1}
+						onKeyDown={(e: KeyboardEvent<SVGAElement>) => isEditable && handleSpace(i + 1, e)}
+					/>
+				</span>
 			);
 		});
 		setRatingArray(updatedArray);
+	};
+
+	//перестраиваем рейтинг
+	const changeDisplay = (i: number):void => {
+		if(!isEditable) return;
+		constructRating(i);
+	};
+
+	//при клике прокидываем i в pages/index.ts
+	const onclick = (i: number): void => {
+		if(!isEditable || !setRating) return;
+		setRating(i);
+	};
+
+	//при использовании навигации и нажатии пробел прокидываем i в pages/index.ts 
+	const handleSpace = (i: number, e: KeyboardEvent<SVGAElement>) => {
+		if(e.code != 'Space' || !setRating) return;
+		setRating(i);
 	};
 
 	return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,13 @@ import { useEffect, useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
-  const [counter, setCounter] = useState<number>(0);
-
-  useEffect(() => {
-    console.log(counter);
-  });
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <>
-      <Htag tag='h1'>{counter}</Htag>
       <Htag tag='h2'>Текст</Htag>
       <Htag  tag='h3'>Текст</Htag>
-      <Button apperrance='primary' arrow='right' className='aa' onClick={():void => setCounter(counter + 1)}>Кнопка</Button>
+      {/* <Button apperrance='primary' arrow='right' className='aa' onClick={():void => setCounter(counter + 1)}>Кнопка</Button> */}
       <Button apperrance='ghost' arrow='right'>Кнопка</Button>
       <P size='l'>123</P>
       <Tag color='green' href='123' size='m'>123</Tag>
@@ -21,7 +16,7 @@ export default function Home(): JSX.Element {
       <Tag color='red' size='s'>123</Tag>
       <Tag color='primary' size='s'>123</Tag>
       <Tag color='gray' size='s'>123</Tag>
-      <Rating rating={4}></Rating>
+      <Rating rating={rating} isEditable={true} setRating={setRating}></Rating>
     </>
   );
 }
