1aebcac08727095008c0818829d88875279bee5d
Sergey Neskin <neskin.x@mail.ru>
2022-10-27 22:50:58 +0700

    добавлен Layout

---
+++ layout/Layout.tsx
@@ -0,0 +1,21 @@
+import styles from './Layout.module.css';
+import { LayoutProps } from './Layout.props';
+import cn from 'classnames';
+import { Header } from './Header/Header';
+import { Footer } from './Footer/Footer';
+import { Sidebar } from './Sidebar/Sidebar';
+
+export const Layout = ({ children }: LayoutProps): JSX.Element => {
+	return (
+		<>
+			<Header  />
+			<div>
+				<Sidebar />
+				<div>
+					{children}
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+};
