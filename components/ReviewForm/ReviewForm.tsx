e8da0acbca3d27107ca76a31b9acd1a82b7f4450
Sergey Neskin <neskin.x@mail.ru>
2022-11-06 20:48:16 +0700

    fix error for ReviewForm

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -7,13 +7,41 @@ import { Textarea } from '../Textarea/Textarea';
 import { Button } from '../Button/Button';
 import CloseIcon from './close.svg';
 import { useForm, Controller } from 'react-hook-form';
-import { IReviewForm } from './ReviewForm.interface';
+import { IReviewForm, IReviewSendResponse } from './ReviewForm.interface';
+import axios from 'axios';
+import { API } from '../../helpers/api';
+import { useState } from 'react';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
-	const { register, control, handleSubmit, formState: { errors } } = useForm<IReviewForm>();
+	const { register, control, handleSubmit, formState: { errors }, reset } = useForm<IReviewForm>();
+	const [isSuccess, setIsSuccess] = useState<boolean>(false);
+	const [isError, setIsError] = useState<string>();
 
-	const onSubmit = (data: IReviewForm) => {
-		console.log(errors);
+	const onSubmit = async (formData: IReviewForm) => {
+		try {
+			const { data } = await axios.post<IReviewSendResponse>(API.review.createDemo, {...formData, productId});
+			if(data.message) {
+				setIsSuccess(true);
+				reset();
+			} else {
+				setIsError('Упс, неполадки с сервером');
+			}
+		} catch (e) {
+			if(e instanceof TypeError) {
+				setIsError(e.message);
+			} else if (e instanceof RangeError) {
+			// обработка исключения RangeError
+				setIsError(e.message);
+			} else if (e instanceof EvalError) {
+				// обработка исключения EvalError
+				setIsError(e.message);
+			} else {
+				// обработка остальных исключений
+				console.log(e); // передать обработчику ошибок
+				setIsError('Что то пошло не так');
+			}
+		}
+
 	};
 
 	return (
@@ -56,11 +84,22 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
 					<span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
 				</div>
 			</div>
-			<div className={styles.success}>
-				<div className={styles.successTitle}>Ваш отзыв отправлен</div>
-				<div>Спасибо, ваш отзыва будет опубликован после проверки</div>
-				<CloseIcon className={styles.close}/>
-			</div>
+			{isSuccess &&
+				<div className={styles.success}>
+					<div className={styles.successTitle}>Ваш отзыв отправлен</div>
+					<div>Спасибо, ваш отзыва будет опубликован после проверки</div>
+					<CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+				</div>
+			}
+			{isError &&
+				<div className={styles.error}>
+					<div className={styles.errorTitle}>
+						Что то пошло не так, обновите страницу <br />
+						{isError}
+					</div>
+					<CloseIcon className={styles.errorClose} onClick={() => setIsError(undefined)}/>
+				</div>
+			}
 		</form>
 	);
 };
--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,7 @@ import { withLayout } from '../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../interfaces/menu.interface';
 import { useRouter } from 'next/router';
+import { API } from '../helpers/api';
 
 function Search({ menu }: SearchProps): JSX.Element {
   const router = useRouter();
@@ -20,7 +21,7 @@ export default withLayout(Search);
 
 export const getStaticProps: GetStaticProps<SearchProps> = async () => {
   const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/find', {
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
     firstCategory
   });
   return {
